149217d97c31f816ab8906488742496e593f9996
rkafle <rkafle@localhost>
2025-01-23 17:01:13 -0500

    updated_commit

--- LSP_Spring_2025/src/org/howard/edu/lsp/assignment1/HelloWorld.java
+++ LSP_Spring_2025/src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -1,9 +1,13 @@
 package org.howard.edu.lsp.assignment1;
 
 public class HelloWorld {
+	/**
+	 * Name: Ritesh Kafle
+	 * @param args
+	 */
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Hello World");
 
 	}
 
