401f641303fc2cfa71a4095728470fc3c1c8bd65
rkafle <rkafle@localhost>
2025-04-08 19:50:17 -0400

    updated code

--- LSP_Spring_2025/src/org/howard/edu/lsp/assignment6/IntegerSetException.java
+++ LSP_Spring_2025/src/org/howard/edu/lsp/assignment6/IntegerSetException.java
@@ -1,6 +1,6 @@
 package org.howard.edu.lsp.assignment6;
 /**
- * this class defines a custom exception as IntegerSetException.
+ * This class defines a custom exception as IntegerSetException.
  */
 public class IntegerSetException extends Exception {
     public IntegerSetException(String message) {
